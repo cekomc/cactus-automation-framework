38f6d2293652940c4c81b6a8293fe3a88324e213
Tsvetan Indzhov <tindzhov@shutterfly.com>
2021-03-09 09:49:20 +0200

    Change project structure

---
+++ src/test/java/com/web/tests/BaseTest.java
@@ -0,0 +1,86 @@
+package com.web.tests;
+
+import io.github.bonigarcia.wdm.WebDriverManager;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.chrome.ChromeOptions;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.testng.annotations.AfterTest;
+import org.testng.annotations.BeforeTest;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.util.Properties;
+
+public class BaseTest {
+
+    private static final Logger LOGGER = LoggerFactory.getLogger(BaseTest.class);
+    private WebDriver driver;
+    protected String userName;
+    protected String password;
+    protected String url;
+    private Properties appProperties = new Properties();
+
+    @BeforeTest
+    public void initialize() {
+        try (InputStream inputStream = getClass().getResourceAsStream("/config.properties")) {
+            appProperties.load(inputStream);
+        } catch (IOException e) {
+            LOGGER.info("Unable to find config.properties");
+            e.printStackTrace();
+        }
+        String headless = appProperties.getProperty("headless");
+
+        WebDriverManager.chromedriver().setup();
+        ChromeOptions chromeOptions = new ChromeOptions();
+
+        if ("true".equals(headless)) {
+            chromeOptions.addArguments("--headless", "--disable-gpu", "--window-size=1920,1200",
+                                       "--ignore-certificate-errors");
+            driver = new ChromeDriver(chromeOptions);
+        } else {
+            chromeOptions.addArguments("-start-maximized");
+            driver = new ChromeDriver();
+        }
+        applyProperties();
+    }
+
+
+    private String readFromInputStream(InputStream inputStream)
+        throws IOException {
+        StringBuilder resultStringBuilder = new StringBuilder();
+        try (BufferedReader br
+                 = new BufferedReader(new InputStreamReader(inputStream))) {
+            String line;
+            while ((line = br.readLine()) != null) {
+                resultStringBuilder.append(line).append("\n");
+            }
+        }
+        return resultStringBuilder.toString();
+    }
+
+    public WebDriver getDriver() {
+        return driver;
+    }
+
+    @AfterTest
+    public void close() {
+        if (null != driver) {
+            driver.close();
+            driver.quit();
+        }
+    }
+
+    protected void applyProperties() {
+        userName = appProperties.getProperty("userName.signIn");
+        password = appProperties.getProperty("password.signIn");
+        url = appProperties.getProperty("app.server.hostname.site");
+    }
+
+    protected void loadUrlToTest(String projectURL){
+         getDriver().get(projectURL);
+    }
+}
