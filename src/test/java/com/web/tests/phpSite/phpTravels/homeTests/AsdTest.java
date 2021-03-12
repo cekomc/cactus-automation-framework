c3a33a32ab37f6c876b2365203ad26299dda2d2f
Tsvetan Indzhov <tindzhov@shutterfly.com>
2021-03-12 10:53:17 +0200

    Add another test

--- src/test/java/com/web/tests/phpSite/phpTravels/homeTests/AsdTest.java
+++ src/test/java/com/web/tests/phpSite/phpTravels/homeTests/AsdTest.java
@@ -2,7 +2,7 @@ package com.web.tests.phpSite.phpTravels.homeTests;
 
 import io.github.bonigarcia.wdm.WebDriverManager;
 import org.openqa.selenium.WebDriver;
-import org.openqa.selenium.remote.DesiredCapabilities;
+import org.openqa.selenium.chrome.ChromeOptions;
 import org.openqa.selenium.remote.RemoteWebDriver;
 import org.testng.annotations.Test;
 
@@ -13,12 +13,10 @@ public class AsdTest {
     WebDriver driver;
 
     public void before() throws InterruptedException {
-        DesiredCapabilities desiredCapabilities = new DesiredCapabilities();
-        desiredCapabilities.setBrowserName(DesiredCapabilities.chrome().getBrowserName());
         WebDriverManager.chromedriver().setup();
-        Thread.sleep(60000);
+        ChromeOptions chromeOptions = new ChromeOptions();
         try {
-            driver = new RemoteWebDriver(new URL("http://localhost:4444/wd/hub"), desiredCapabilities);
+            driver = new RemoteWebDriver(new URL("http://localhost:4444/wd/hub"), chromeOptions);
         }catch (MalformedURLException e){
             e.printStackTrace();
         }
